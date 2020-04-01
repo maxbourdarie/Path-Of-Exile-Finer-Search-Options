35b82861d5ec68900a603a0d526004ec0038642b
Mbourdarie <m.maximebourdarie@gmail.com>
2020-04-01 10:13:22 +0200

    added pseudo mod all resists

--- poe-finer-search-options.user.js
+++ poe-finer-search-options.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poe trade finer-search-options
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  enables finer search options in Path of exile official trade site, allowing filtering in/out mods directly from the current search result list
 // @author       Maxime B
 // @match        https://www.pathofexile.com/trade
@@ -17,20 +17,20 @@
     //---------HTML---------//
     const scriptStyle=$(`
         <style>
-            #finer-search-global{color:white;height: 185px;width: 175px;background: #2f2f2f;position: fixed;right: 10px;top: 50px;z-index:1000}
-            #finer-search-global-title{font-weight:bold;margin:40px;}
-            #addFilter{background: rgba(2, 93, 34, 0.75);}
-            #rmvFilter{background: rgba(93, 2, 2, 0.75);}
-            #btns-finer{left:auto;}
-            .finer-global-btn{background: #636363;outline:solid 1px rgb(80, 80, 80);margin:10px 2px;padding: 0 5px;}
-            .finer-global-btn-pm {float: right;padding:0 2px;}
-            .finer-global-btn-pm.minus {background: red;padding: 0 5px;}
-            .finer-global-btn-pm.plus {background: green;margin-left:5px;}
-            .finer-filterable:hover {background: rgba(255, 255, 255, 0.2);}
-            .finer-filtered{position: relative;}
-            .finer-filtered-overlay {z-index: -1;position: absolute;width: 100%;height: 100%;background: rgba(0, 136, 0, .25);top: 0;}
-            .btn-finer-search {padding: 0 10px;color: white;outline: solid 1px grey;}
-            .hand{cursor:pointer;}
+            #finer-search-global{ color: white;    height: 210px;    width: 175px;    background: #2f2f2f;    position: fixed;    right: 10px;    top: 50px;    z-index: 1000 }
+            #finer-search-global-title{ font-weight: bold;    margin: 40px; }
+            #addFilter{ background: rgba(2,  93, 34, 0.75); }
+            #rmvFilter{ background: rgba(93, 2,  2,  0.75); }
+            #btns-finer{ left: auto; }
+            .finer-global-btn{ background: #636363;    outline: solid 1px rgb(80, 80, 80);    margin: 10px 2px;    padding: 0 5px; }
+            .finer-global-btn-pm { float: right;    padding: 0 2px; }
+            .finer-global-btn-pm.minus { background: red;    padding: 0 5px; }
+            .finer-global-btn-pm.plus { background: green;    margin-left:5px; }
+            .finer-filterable:hover { background: rgba(255, 255, 255, 0.2); }
+            .finer-filtered{ position: relative; }
+            .finer-filtered-overlay { z-index: -1;    position: absolute;    width: 100%;    height: 100%;    background: rgba(0, 136, 0, .25);    top: 0; }
+            .btn-finer-search { padding: 0 10px;    color: white;    outline: solid 1px grey;}
+            .hand{ cursor: pointer; }
         </style>
     `);
     const globalFiner = $(`
@@ -38,6 +38,11 @@
             <span id="finer-search-global-title">add to filters</span>
             <div class="finer-global-btn" data-type="life,cold,fire,ligt">
                 all res & life
+                <span title="increase min value by 10" class="finer-global-btn-pm plus hand">+</span>
+                <span title="decrease min value by 10" class="finer-global-btn-pm minus hand">-</span>
+            </div>
+            <div class="finer-global-btn" data-type="allR">
+                all resistances
                 <span title="increase min value by 10" class="finer-global-btn-pm plus hand">+</span>
                 <span title="decrease min value by 10" class="finer-global-btn-pm minus hand">-</span>
             </div>
@@ -78,18 +83,19 @@
 
 
 //---------UTILS METHODS---------//
-    const modMap={life:"total_life",cold:"total_cold_resistance",fire:"total_fire_resistance",ligt:"total_lightning_resistance",move:"increased_movement_speed"};
+    const modMap={life:"total_life",cold:"total_cold_resistance",fire:"total_fire_resistance",ligt:"total_lightning_resistance",move:"increased_movement_speed",allR:"total_elemental_resistance"};
 
     const createFilter = _type => _type && ({ id: _type, value: {}, disabled: !1 });
 
     const finder = (e,v) => e.$vnode.tag.includes(v);
+
     const findVueItem = tags => tags.reduce((a,v)=> a.$children.find(e => finder(e,v)), window.app);
 
     const ItemResultPanelVueItem = () => findVueItem(["item-results-panel"]);
 
     const findVueResultItem = _itemId => findVueItem(["item-results-panel", "resultset"]).$children.find(e => e.itemId === _itemId);
 
-    const ItemSearchGroupsVueItems = _type => findVueItem(["item-search-panel", "item-filter-panel"]).$children.filter(e => finder(e,"stat-filter-group") && _type ? e.group.type === _type : true);
+    const ItemSearchGroupsVueItems = _type => findVueItem(["item-search-panel", "item-filter-panel"]).$children.filter(e => finder(e,"stat-filter-group") && (_type ? e.group.type === _type : true));
 
 //---------ACTUAL CODE DOING STUFF AND ALL---------//
     $("body").append(scriptStyle);//append css to body
@@ -108,7 +114,7 @@
         .on('click','#addFilter', function(e){ addOrRemoveFilter(e, true , this) })
         .on('click','#rmvFilter', function(e){ addOrRemoveFilter(e, false, this) })
         //-----------step 4 : on button click in the global div, add the corresponding mods to the search
-        .on('click','#finer-search-global > .finer-global-btn', function(e){
+        .on('click','#finer-search-global > .finer-global-btn > span', function(e){
             addPseudoMods([...$(e.target).parent().data("type").split(',')], $(e.target).hasClass("plus"))
         })
 
@@ -155,7 +161,7 @@
                 else ISG.removeFilter(modIndex);
             }
             else {
-                ISG.selectFilter(createFilter(`pseudo.pseudo_${modMap[hash]}`));
+                ISG.selectFilter(createFilter(reHashed));
             }
         });
         window.app.save(!0);
